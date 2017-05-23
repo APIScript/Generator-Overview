f91ce84a0bc955c5c958f7f596480c92b296c009
Cytren <contact@cytren.com>
2017-05-23 20:44:53 +0100

    Updated to new type reading system

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,6 +1,7 @@
 
-import * as chalk from 'chalk';
-import * as apiscript from 'apiscript';
+import * as chalk from "chalk";
+import * as apiscript from "apiscript";
+import {PropertyType} from "apiscript";
 
 export class OverviewGenerator implements apiscript.Generator {
 
@@ -47,10 +48,6 @@ export class OverviewGenerator implements apiscript.Generator {
 
 }
 
-function clear() {
-    print('\x1bc');
-}
-
 function print(message: any = '') {
     console.log(' ' + message);
 }
@@ -85,12 +82,12 @@ function printProperties(propertyHolder: apiscript.Endpoint | apiscript.Entity) 
             let message = '';
             if (property.isOptional) { message += `Optional `; }
 
-            message += property.type;
-            message += ` ${property.name}`;
+            message += `${property.name} `;
+            message += `${propertyTypeToString(property.type)}`;
 
             if (property.defaultValue) {
 
-                if (property.type.isString) {
+                if (property.type.asPrimitive && property.type.asPrimitive.asString) {
                     message += ` = "${property.defaultValue}"`;
                 } else {
                     message += ` = ${property.defaultValue}`;
@@ -100,6 +97,51 @@ function printProperties(propertyHolder: apiscript.Endpoint | apiscript.Entity) 
             if (property.constraints) { message += ` (${property.constraints})`; }
             printValue(message);
         });
+    }
+}
+
+function propertyTypeToString(type: PropertyType): string {
+
+    if (type.asPrimitive) {
+        let primitive = type.asPrimitive;
+
+        if (primitive.asInteger) { return 'Integer'; }
+        if (primitive.asFloat) { return 'Float'; }
+        if (primitive.asBoolean) { return 'Boolean'; }
+        if (primitive.asString) { return 'String'; }
+
+    } else if (type.asCollection) {
+        let collection = type.asCollection;
+
+        if (collection.asList) {
+            let list = collection.asList;
+            return `[${propertyTypeToString(list.type)}]`;
+        }
+
+        if (collection.asSet) {
+            let set = collection.asSet;
+            return `<${propertyTypeToString(set.type)}>`;
+        }
+
+        if (collection.asMap) {
+            let map = collection.asMap;
+            return `<${propertyTypeToString(map.keyType)} -> ${propertyTypeToString(map.keyType)}>`;
+        }
+
+    } else if (type.asCustom) {
+        return type.asCustom.type;
+
+    } else if (type.asClosure) {
+        let properties = type.asClosure.properties;
+        let result = '{ ';
+
+        properties.forEach((property, index) => {
+            result += property.name + ': ' + propertyTypeToString(property.type);
+            if (index < properties.length - 1) { result += ', '; }
+        });
+
+        result += ' }';
+        return result;
     }
 }
 
